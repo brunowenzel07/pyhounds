9c28b01433372c97b5f41260e7ac959e948f222c
Patrick Acioli <acioli@protonmail.com>
2018-09-27 11:15:47 -0300

    finalizando naive bayes

--- naive.py
+++ naive.py
@@ -7,24 +7,33 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report 
 
 # Importa os dados para treino
 train_df = pd.read_csv("data/comments.csv", header=None, names=["comment", "position"])
+test_df = pd.read_csv("data/comments_test.csv", header=None, names=["comment", "position"])
 
 # Bag of Words
 tfidf = TfidfTransformer()
 bow = CountVectorizer()
-bow.fit(train_df["comment"])
+bow.fit(test_df.append(train_df)["comment"])
 
-# treinamento
+# instanciando classificador
+nb = MultinomialNB(alpha=1.0)
+
+# treinamento, transformação do set de trainamento
 train_X_bow = bow.transform(train_df["comment"])
 tfidf.fit(train_X_bow)
 train_X_tfidf = tfidf.transform(train_X_bow)
 train_y = train_df["position"]
-
-nb = MultinomialNB(alpha=1.0)
 nb.fit(train_X_tfidf, train_y)
-y_pred = nb.predict(train_X_tfidf)
 
+# criando samples de test 
+test_X_bow = bow.transform(test_df["comment"])
+tfidf.fit(test_X_bow)
+test_X_tfidf = tfidf.transform(test_X_bow)
+test_y = test_df["position"]
 
-print(accuracy_score(train_y, y_pred))
+y_pred = nb.predict(test_X_tfidf)
+
+print(classification_report(test_y, y_pred))
